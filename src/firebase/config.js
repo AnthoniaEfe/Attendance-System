b94ab93e36ca09fb017bd72380c61d7e30207bf7
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-03-03 23:52:16 +0100

    config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = firebaseConfig.initializeApp(firebaseConfig);
 // firebase.initializeApp(firebaseConfig);
 console.log("firebase initailized");
 
