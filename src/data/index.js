f02296beb857fc662faf46e1bbbc697b54912114
raphaelohai5 <sameleno6@gmail.com>
2023-06-04 19:05:23 +0100

    data added

---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    name: "gggg",
+    matNum: "17/engo5/027",
+    department: "",
+    level: "",
+  },
+  {
+    name: "gggg",
+    matNum: "17/engo5/027",
+    department: "",
+    level: "",
+  },
+  {
+    name: "gggg",
+    matNum: "17/engo5/027",
+    department: "",
+    level: "",
+  },
+  {
+    name: "gggg",
+    matNum: "17/engo5/027",
+    department: "",
+    level: "",
+  },
+  {
+    name: "gggg",
+    matNum: "17/engo5/027",
+    department: "",
+    level: "",
+  },
+];
